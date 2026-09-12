1215ae7500fd54f9fdb4c11a53601258d8a15c9e
Ryan Mole <ryan.mole@awi.de>
2026-09-13 00:09:50 +0200

    build: implement particle initialisation in main

--- CAdvect/CAdvect.cpp
+++ CAdvect/CAdvect.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <filesystem>
 #include "Fields.h"
+#include "Particles.h"
 
 int main(int argc, char* argv[]) {
-
-    std::cout << "There were " << argc << " command line arguments:" << "\n";
 
 //  loop through args looking for the input file flag -i and assign to path
     std::filesystem::path filepath;
@@ -30,27 +29,52 @@ int main(int argc, char* argv[]) {
     if (!filepath.empty() && !std::filesystem::exists(filepath)) {
         std::cerr << "WARNING: " << filepath << " does not exist!\n";
     }
-    auto extension = filepath.extension();
-    std::cout << extension << "\n";
-    std::cout << extension.c_str() << "\n";
-    std::cout << (extension == ".nc") << "\n";
+    if (found_input) {
+        auto extension = filepath.extension();
+        std::cout << extension << "\n";
+        std::cout << extension.c_str() << "\n";
+        std::cout << (extension == ".nc") << "\n";
+    }
     
-    // load data
-    std::cout << "trying to initialise a field... \n\n";
+    // load field data
+    std::cout << "Initialising u, v fields...\n";
 
     int nx = 10;
     int ny = 10;
     double mag = 10.;
     Field field = single_gyre(nx, ny, mag, Direction::CounterClockwise);
 
+    /*
     for (int row = 0; row < (ny); row++) {
         for (int col = 0; col < (nx); col++) {
         std::cout << field.v[(row * nx) + col] << ", ";
         }
     std::cout << "\n";
+
+   
     }
+     */
 
     // initialise particles
+    std::cout << "Initialising particles... ";
+    const std::array<double, 2> start_pos = {0.5, 0.1};
+    const std::array<double, 2> end_pos = {0.5, 0.9};
+    const int N = 25;
+    std::vector<Particle> particles = from_line(start_pos, end_pos, N);
+
+    /*
+    std::cout << "Initialised the following particles:\n";
+    for (int i=0; i < N; i++) {
+        std::cout << i + 1 << "\t";
+        Particle particle = particles[i];
+        std::cout << "(" << particle.x << ", " << particle.y << ")\n";
+    }
+    */
+
+    std::cout << "Initialised " << particles.size() << " particles:\n";
+        
+        
+    
 
     // timestep loop
 
