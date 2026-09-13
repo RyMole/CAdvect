b4e8fda05a18051c5000e9dc868f17def8ff54b4
Ryan Mole <ryan.mole@awi.de>
2026-09-13 16:38:54 +0200

    fix: revert from_line to accept vectors

--- CAdvect/Particles.h
+++ CAdvect/Particles.h
@@ -9,6 +9,6 @@ struct Particle { double x, y;};
 
 // return vector of particles given two coordinates and number of particles N
 // coords must be arrays of (x, y)
-std::vector<Particle> from_line(const std::array<double, 2>& start_pos, const std::array<double, 2>& end_pos, int N);
+std::vector<Particle> from_line(const std::vector<double>& start_pos, const std::vector<double>& end_pos, int N);
 
 #endif
